647ebb4dd405270cc13c0c085adbe730fc5117f2
Tom <htge@xtremeprog.com>
2015-03-03 10:25:58 +0800

    Add required sources

---
+++ utils/GizDataAccess/GizDataAccess.framework/Versions/A/Headers/GizDataAccessDifinitions.h
@@ -0,0 +1,118 @@
+//
+//  GizDataAccessDifinitions.h
+//  GizDataAccess
+//
+//  Created by xpg on 14/12/30.
+//  Copyright (c) 2014年 xpg. All rights reserved.
+//
+
+#ifndef GizDataAccess_GizDataAccessDifinitions_h
+#define GizDataAccess_GizDataAccessDifinitions_h
+
+#define GIZ_ERRORMSG_SUCCESS        @"Success"
+#define GIZ_ERRORMSG_FAILED         @"Failed"
+
+/**
+ GizDataAccessErrorCode枚举，描述了SDK提供的所有错误码定义。
+ */
+typedef NS_ENUM(NSInteger, GizDataAccessErrorCode)
+{
+    /**
+     无错误
+     */
+    kGizDataAccessErrorNone                      = 0,
+    /**
+     连接失败
+     */
+    kGizDataAccessErrorConnectionFailed          = 8001,
+    /**
+     连接超时
+     */
+    kGizDataAccessErrorConnectionTimeout         = 8002,
+    /**
+     参数错误
+     */
+    kGizDataAccessErrorInvalidParameters         = 8003,
+    /**
+     Product Key 非法
+     */
+    kGizDataAccessErrorInvalidProductKey         = 9002,
+    /**
+     App ID 非法
+     */
+    kGizDataAccessErrorInvalidAppID              = 9003,
+    /**
+     Token 非法
+     */
+    kGizDataAccessErrorInvalidToken              = 9004,
+    /**
+     用户不存在
+     */
+    kGizDataAccessErrorUserNotExists             = 9005,
+    /**
+     Token 过期
+     */
+    kGizDataAccessErrorTokenExpired              = 9006,
+    /**
+     服务器错误
+     */
+    kGizDataAccessErrorServer                    = 9008,
+    /**
+     手机验证码过期
+     */
+    kGizDataAccessErrorCodeExpired               = 9009,
+    /**
+     手机验证码错误
+     */
+    kGizDataAccessErrorCodeInvalid               = 9010,
+    /**
+     数据格式错误
+     */
+    kGizDataAccessErrorFormInvalid               = 9015,
+    /**
+     手机号不可用
+     */
+    kGizDataAccessErrorPhoneUnavaliable          = 9018,
+    /**
+     用户名不可用
+     */
+    kGizDataAccessErrorUsernameUnavaliable       = 9019,
+    /**
+     用户名或密码错误
+     */
+    kGizDataAccessErrorUsernameOrPasswordError   = 9020,
+    /**
+     其他情况
+     */
+    kGizDataAccessErrorOtherwise                 = 9999
+};
+
+typedef NS_ENUM(NSInteger, GizDataAccessThirdAccountType)
+{
+    /**
+     * 使用新浪账号登陆
+     */
+    kGizDataAccessThirdAccountTypeSINA           = 0,
+    /**
+     * 使用百度账号登陆
+     */
+    kGizDataAccessThirdAccountTypeBAIDU
+};
+
+typedef NS_ENUM(NSInteger, GizDataAccessAccountType)
+{
+    /**
+     * 普通账号
+     */
+    kGizDataAccessAccountTypeNormal = 0,
+    /**
+     * 手机账号
+     */
+    kGizDataAccessAccountTypePhone,
+    /**
+     * 邮箱账号
+     */
+    kGizDataAccessAccountTypeEmail
+};
+
+#endif
---
+++ utils/GizDataAccess/GizDataAccess.framework/Versions/A/Headers/GizDataAccessLogin.h
@@ -0,0 +1,228 @@
+//
+//  GizDataAccessLogin.h
+//  GizDataAccess
+//
+//  Created by xpg on 14/12/30.
+//  Copyright (c) 2014年 xpg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <GizDataAccess/GizDataAccessDifinitions.h>
+
+@class GizDataAccessLogin;
+
+/**
+ GizDataAccessLoginDelegate是GizDataAccessLogin类的委托协议，为APP开发者处理用户登录结果提供委托函数。
+ */
+@protocol GizDataAccessLoginDelegate <NSObject>
+@optional
+
+/**
+ *
+ * 登录回调
+ *
+ * @param login 当前登录实例
+ *
+ * @param uid 登录成功后得到的id，如果失败则为nil
+ *
+ * @param token 登录成功后得到的token（用户鉴权令牌），如果失败则为nil
+ *
+ * @param result 登录结果，成功或失败。详情参考 GizDataAccessErrorCode 定义
+ *
+ * @param message 错误信息，成功为 "Success"
+ *
+ * @see 触发函数：
+ * [GizDataAccessLogin loginAnonymous]，
+ * [GizDataAccessLogin login:password:]，
+ * [GizDataAccessLogin loginWithThirdAccountType:uid:token:]
+ *
+ */
+- (void)gizDataAccess:(GizDataAccessLogin *)login didLogin:(NSString *)uid token:(NSString *)token result:(GizDataAccessErrorCode)result message:(NSString *)message;
+
+/**
+ *
+ * 注册回调
+ *
+ * @param login 当前登录实例
+ *
+ * @param uid 登录成功后得到的id，如果失败则为nil
+ *
+ * @param token 登录成功后得到的token（用户鉴权令牌），如果失败则为nil
+ *
+ * @param result 登录结果，成功或失败。详情参考 GizDataAccessErrorCode 定义
+ *
+ * @param message 错误信息，成功为 "Success"
+ *
+ * @see 触发函数：
+ * [GizDataAccessLogin registerUser:password:code:]，
+ *
+ */
+- (void)gizDataAccess:(GizDataAccessLogin *)login didRegisterUser:(NSString *)uid token:(NSString *)token result:(GizDataAccessErrorCode)result message:(NSString *)message;
+
+/**
+ *
+ * 获取验证码回调
+ *
+ * @param login 当前登录实例
+ *
+ * @param result 登录结果，成功或失败。详情参考 GizDataAccessErrorCode 定义
+ *
+ * @param message 错误信息，成功为 "Success"
+ *
+ * @see 触发函数：
+ * [GizDataAccessLogin requestSendVerifyCode:]，
+ *
+ */
+- (void)gizDataAccess:(GizDataAccessLogin *)login didRequestSendVerifyCode:(GizDataAccessErrorCode)result message:(NSString *)message;
+
+/**
+ *
+ * 修改/重置密码的回调
+ *
+ * @param login 当前登录实例
+ *
+ * @param result 登录结果，成功或失败。详情参考 GizDataAccessErrorCode 定义
+ *
+ * @param message 错误信息，成功为 "Success"
+ *
+ * @see 触发函数：
+ * [GizDataAccessLogin changeUserPassword:oldPassword:newPassword:]
+ * [GizDataAccessLogin resetPassword:code:newPassword:]
+ *
+ */
+- (void)gizDataAccess:(GizDataAccessLogin *)login didChangeUserPassword:(GizDataAccessErrorCode)result message:(NSString *)message;
+
+/**
+ *
+ * 修改实名用户为手机或邮箱用户
+ *
+ * @param login 当前登录实例
+ *
+ * @param result 登录结果，成功或失败。详情参考 GizDataAccessErrorCode 定义
+ *
+ * @param message 错误信息，成功为 "Success"
+ *
+ * @see 触发函数：
+ * [GizDataAccessLogin changeUser:username:code:]
+ *
+ */
+- (void)gizDataAccess:(GizDataAccessLogin *)login didChangeUser:(GizDataAccessErrorCode)result message:(NSString *)message;
+
+@end
+
+/**
+ GizDataAccessLogin类为APP开发者提供登录机智云的函数。
+ 
+ 我们提供匿名登录方式登录到机智云，这种方式不需要输入用户名密码，使APP的用户登录过程更简单。
+ */
+@interface GizDataAccessLogin : NSObject
+
+- (id)init NS_UNAVAILABLE;
+
+/**
+ *
+ * 初始化GizDataAccessLogin类实例。只有在类实例初始化时才能设置相应的回调，才可以得到用户登录的结果
+ *
+ * @param delegate 使用此类方法相应的回调
+ *
+ * @return GizDataAccessLogin类实例
+ *
+ */
+- (id)initWithDelegate:(id<GizDataAccessLoginDelegate>)delegate;
+
+/**
+ *
+ * 向指定的手机号发送验证码
+ *
+ * @param phone 手机号
+ *
+ */
+- (void)requestSendVerifyCode:(NSString *)phone;
+
+/**
+ *
+ * 使用用户名、密码注册普通用户
+ *
+ * @param username 用户名，可以是手机号、邮箱和其他
+ * @param password 密码
+ * @param code 如果用户名是手机，则输入验证码
+ *
+ * @see 对应的回调接口：[GizDataAccessLoginDelegate gizDataAccess:didRegisterUser:token:result:message:]
+ *
+ */
+- (void)registerUser:(NSString *)username password:(NSString *)password code:(NSString *)code accountType:(GizDataAccessAccountType)accountType;;
+
+/**
+ *
+ * 匿名登录。系统会自动生成一个用户名，登录成功就会生成对应的 uid 和 token
+ *
+ * @see 对应的回调接口：[GizDataAccessLoginDelegate gizDataAccess:didLogin:token:result:message:]
+ *
+ */
+- (void)loginAnonymous;
+
+/**
+ *
+ * 第三方用户登录。使用已注册好的第三方用户和密码，用第三方 SDK 获取到相应的 uid, token 即可登录
+ *
+ * @param thirdAccountType 第三方账号类型，目前支持新浪、百度
+ * @param uid 通过第三方 SDK 得到的用户 id
+ * @param token 通过第三方 SDK 得到的用户 token
+ *
+ * @see 对应的回调接口：[GizDataAccessLoginDelegate gizDataAccess:didLogin:token:result:message:]
+ *
+ */
+- (void)loginWithThirdAccountType:(GizDataAccessThirdAccountType)thirdAccountType uid:(NSString *)uid token:(NSString *)token;
+
+/**
+ *
+ * 用户登录
+ *
+ * @param username 用户名，可以使用普通用户、手机用户、邮箱用户登录
+ * @param password 密码
+ *
+ * @see 对应的回调接口：[GizDataAccessLoginDelegate gizDataAccess:didLogin:token:result:message:]
+ *
+ */
+- (void)login:(NSString *)username password:(NSString *)password;
+
+/**
+ *
+ * 修改密码
+ *
+ * @param token         登录成功后得到的token（用户鉴权令牌）
+ * @param oldPassword   旧密码
+ * @param newPassword   新密码
+ *
+ * @see 对应的回调接口：[GizDataAccessLoginDelegate gizDataAccess:didChangeUserPassword:message:]
+ *
+ */
+- (void)changeUserPassword:(NSString *)token oldPassword:(NSString *)oldPassword newPassword:(NSString *)newPassword;
+
+/**
+ *
+ * 重置手机或邮箱用户密码
+ *
+ * @param username      只能是手机号或者邮箱
+ * @param code          当输入手机号时，需要输入手机验证码
+ * @param newPassword   当输入手机号时，需要输入新密码
+ *
+ * @see 对应的回调接口：[GizDataAccessLoginDelegate gizDataAccess:didChangeUserPassword:message:]
+ *
+ */
+- (void)resetPassword:(NSString *)username code:(NSString *)code newPassword:(NSString *)newPassword accountType:(GizDataAccessAccountType)accountType;
+
+/**
+ *
+ * 修改实名用户为手机用户
+ *
+ * @param token         登录成功后得到的token（用户鉴权令牌）
+ * @param username      用户名，只能是手机号和邮箱
+ * @param code          用户名是手机时，填写手机验证码
+ *
+ * @see 对应的回调接口：[GizDataAccessLoginDelegate gizDataAccess:didChangeUser:message:]
+ *
+ */
+- (void)changeUser:(NSString *)token username:(NSString *)username code:(NSString *)code accountType:(GizDataAccessAccountType)accountType;
+
+@end
